0d15ee9a9a74c4230036ae11135ad9537002e675
Hugo Gicquel <hugogicquel@me.com>
2021-07-04 12:26:02 +0200

    ex03

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -10,21 +10,77 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include <stdio.h>
+
+int	ft_isspace(char c)
+{
+	if (c == '\t')
+		return (1);
+	if (c == '\n')
+		return (1);
+	if (c == '\v')
+		return (1);
+	if (c == '\f')
+		return (1);
+	if (c == ' ')
+		return (1);
+	return (0);
+}
+
+int	ft_neg(char	*str, int *i)
+{
+	int	s;
+
+	s = 0;
+	while (str[*i] == '-' || str[*i] == '+')
+	{
+		if (str[*i] == '-')
+			s = !s;
+		(*i)++;
+	}
+	return (s);
+}
+
+int	ft_satoi(char *str, int *i, int s)
+{
+	int	n;
+
+	n = 0;
+	while (str[*i] >= '0' && str[*i] <= '9')
+	{
+		n *= 10;
+		if (n < 0)
+			n -= str[*i] - '0';
+		else
+			n += str[*i] - '0';
+		if (s)
+		{
+			n *= -1;
+			s = 0;
+		}
+		(*i)++;
+	}
+	return (n);
+}
+
 int	ft_atoi(char *str)
 {
 	int		i;
-	char	c;
 	int		n;
 	int		s;
 
-	while(1)
-	{
-		c = str[i];
-		if (c == '-')
-			s = -1;
-		if (c == '+')
-			s = 1;
-		if (c < '0' || c > '9')
-			break ;
-	}
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	s = ft_neg(str, &i);
+	n = ft_satoi(str, &i, s);
+	return (n);
 }
+
+// int	main(void)
+// {
+// 	char	str[] = "   --+-2147483647";
+
+// 	printf("%d\n", ft_atoi(str));
+// 	return (0);
+// }
