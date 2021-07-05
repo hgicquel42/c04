caf63feb1d9ebb8ea4cc60f00e033a2ec2bc34bd
Hugo Gicquel <hugogicquel@me.com>
2021-07-05 14:39:28 +0200

    tests

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -75,16 +75,3 @@ int	ft_atoi(char *str)
 	n = ft_satoi(str + i, s);
 	return (n);
 }
-
-// int	main(void)
-// {
-// 	printf("42:%d\n", ft_atoi("  \n  42t4457"));
-// 	printf("-42:%d\n", ft_atoi(" --+-42sfs:f545"));
-// 	printf("0:%d\n", ft_atoi("\0 1337"));
-// 	printf("0:%d\n", ft_atoi("-0"));
-// 	printf("0:%d\n", ft_atoi(" - 1 3 2 5 6 3 2 1 6 7"));
-// 	printf("-1325632167:%d\n", ft_atoi("-1325632167"));
-// 	printf("-100:%d\n", ft_atoi("-100"));
-// 	printf("min:%d\n", ft_atoi("\t---+2147483648"));
-// 	printf("max:%d\n", ft_atoi("\v2147483647"));
-// }
